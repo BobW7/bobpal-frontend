dd4cae578f97cc55652f7f9dc516b623d31d7f03
BobW7 <1186717311@qq.com>
2023-10-23 19:43:18 +0800

    Initial commit

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      fixed
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+  />
+
+</template>
+
+<script setup>
+const onClickLeft = () => alert('left');
+const onClickRight = () => alert('right');
+
+</script>
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { NavBar } from 'vant';
+import { Button } from 'vant';
+import './style.css'
+// @ts-ignore
+import App from './App.vue'
+
+const app = createApp(App);
+app.use(NavBar);
+app.use(Button);
+app.mount('#app')
