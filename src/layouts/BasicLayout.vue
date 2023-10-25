65290d4b2758aceec5615829ab9e93cab4ef6801
BobW7 <1186717311@qq.com>
2023-10-25 17:12:10 +0800

    成功整合路由

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -14,17 +14,12 @@
   </van-nav-bar>
   <van-search v-model="value" placeholder="请输入搜索关键词" />
   <div id="content">
-    <template v-if="active === 'index'">
-      <Index/>
-    </template>
-    <template v-if="active === 'team'">
-      <Team/>
-    </template>
+    <router-view/>
   </div>
-  <van-tabbar v-model="active" @change="onChange">
-    <van-tabbar-item icon="home-o" name="index">主页</van-tabbar-item>
-    <van-tabbar-item icon="search" name="team">队伍页</van-tabbar-item>
-    <van-tabbar-item icon="friends-o" name="user">个人</van-tabbar-item>
+  <van-tabbar route @change="onChange">
+    <van-tabbar-item icon="home-o" to="/" name="index">主页</van-tabbar-item>
+    <van-tabbar-item icon="search" to="/team" name="team">队伍页</van-tabbar-item>
+    <van-tabbar-item icon="friends-o" to="/user" name="user">个人</van-tabbar-item>
   </van-tabbar>
 
 </template>
