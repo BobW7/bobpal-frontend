3523e7400a96c3d1fd780c0532bc8e0f771787a4
BobW7 <1186717311@qq.com>
2023-10-25 16:59:34 +0800

    提出路由到route.ts文件

---
+++ src/config/route.ts
@@ -0,0 +1,10 @@
+import Index from "../pages/Index.vue";
+import Team from "../pages/Team.vue";
+
+//定义一些路由
+const routes=[
+    {path:'/',component: Index},
+    {path:'/team',component: Team},
+]
+
+export default routes;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { createApp } from 'vue'
 import { NavBar } from 'vant';
 import { Button } from 'vant';
+//引入router组件
+import * as VueRouter from 'vue-router';
 
 import App from './App.vue'
-
+import routes from "./config/route.ts";
 const app = createApp(App);
 app.use(NavBar);
 app.use(Button);
+const router = VueRouter.createRouter({
+    //内部提供history模式的实现
+    history:VueRouter.createMemoryHistory(),
+    routes,
+})
+app.use(router);
 app.mount('#app')
+
+
