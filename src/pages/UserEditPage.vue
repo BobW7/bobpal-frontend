ef05b4bd8bf0fd686cf9e3803b0c534cdde2ab7b
BobW7 <1186717311@qq.com>
2023-10-26 16:46:40 +0800

    编写用户页的跳转到编辑页面功能，注意路由信息的发送和接收操作。到后期可以封装字典

---
+++ src/pages/UserEditPage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+import {ref} from "vue";
+const route = useRoute();
+console.log(route.query)
+
+//从路由参数中获取编辑信息，存储在 editUser 中
+const editUser = ref({
+  editKey: route.query.editKey,
+  editName: route.query.editName,
+  currentValue: route.query.currentValue
+})
+const onSubmit = (values) => {
+  // todo 把editKey,editName,currentValue提交到后台
+  console.log('onSubmit',values)
+};
+</script>
+
+<template>
+  <van-form @submit="onSubmit">
+      <van-field
+          v-model="editUser.currentValue"
+          :name="`${editUser.editKey}`"
+          :label="`${editUser.editName}`"
+          :placeholder="`请输入${editUser.editName}`"
+      />
+
+    <div style="margin: 16px;">
+      <van-button round block type="primary" native-type="submit">
+        提交
+      </van-button>
+    </div>
+  </van-form>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,28 +1,43 @@
-<script setup>
+<script setup lang="ts">
+import {useRoute, useRouter} from "vue-router";
+
 const user = {
   id: 1,
   username: 'Bob',
   userAccount: 'kingBob',
-  avatarUrl: 'https://nimg.ws.126.net/?url=https%3A%2F%2Fdingyue.ws.126.net%2F2023%2F0727%2F19b6bd99j00ryg52n005kd000sg00wqp.jpg&thumbnail=300x190&quality=95&type=jpg',
+  avatarUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202004%2F30%2F20200430162112_GjeHt.thumb.1000_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1700897506&t=25064d1a432e83f1e671aebedfea6043',
   gender: '男',
   phone: '66185',
   email: '12366@qq.com',
   planetCode: 6666,
   createTime: new Date(),
 }
+
+const router = useRouter();
+
+const toEdit = (editKey: string,editName: string,currentValue: string) => {
+  router.push({
+    path: '/user/edit',
+    query: {
+      editKey,
+      editName,
+      currentValue,
+    }
+  })
+}
 </script>
 
 <template>
-  <van-cell title="昵称" is-link to="/user/edit" :value="user.username" />
-  <van-cell title="账号" is-link to="/user/edit" :value="user.userAccount" />
-  <van-cell title="头像" is-link to="/user/edit">
+  <van-cell title="昵称" is-link to="/user/edit" :value="user.username" @click="toEdit('nickName',  '昵称', user.username)"/>
+  <van-cell title="账号" is-link to="/user/edit" :value="user.userAccount" @click="toEdit('account',  '账号', user.userAccount)"/>
+  <van-cell title="头像" is-link to="/user/edit" @click="toEdit('avatar','头像',user.avatarUrl)">
     <img style="height: 100px" :src="user.avatarUrl">
   </van-cell>
-  <van-cell title="性别" is-link to="/user/edit" :value="user.gender" />
-  <van-cell title="电话" is-link to="/user/edit" :value="user.phone" />
-  <van-cell title="邮箱" is-link to="/user/edit" :value="user.email" />
+  <van-cell title="性别" is-link to="/user/edit" :value="user.gender" @click="toEdit('gender',  '性别', user.gender)"/>
+  <van-cell title="电话" is-link to="/user/edit" :value="user.phone" @click="toEdit('phoneNum',  '电话', user.phone)"/>
+  <van-cell title="邮箱" is-link to="/user/edit" :value="user.email" @click="toEdit('email',  '邮箱', user.email)"/>
   <van-cell title="星球编号" :value="user.planetCode" />
-  <van-cell title="注册时间" :value="user.createTime" />
+  <van-cell title="注册时间" :value="user.createTime.toDateString()" />
 
 </template>
 
