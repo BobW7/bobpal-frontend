9da1581aa5f6d31cd8843b51a37d6f59367fa5c1
BobW7 <1186717311@qq.com>
2023-10-29 19:27:04 +0800

    开发基本登录，还未完全完善

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -18,7 +18,7 @@ myAxios.interceptors.response.use(function (response) {
     console.log("成功收到响应",response)
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
-    return response;
+    return response.data;
 }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
