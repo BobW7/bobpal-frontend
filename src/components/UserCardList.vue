8eb19908fa33352136c472584455af49c4cd8673
BobW7 <1186717311@qq.com>
2023-11-20 09:42:28 +0800

    添加加载特效

--- src/components/UserCardList.vue
+++ src/components/UserCardList.vue
@@ -3,17 +3,19 @@
 import {UserType} from "../modules/user";
 
 interface UserCardListProps{
+  loading:boolean;
   userList: UserType[];
 }
 //props中能得到父组件传递来的属性，比如userList
  const props =withDefaults(defineProps<UserCardListProps>() , {
-   userList: [] as UserType,
+   loading:true,
+   userList: [] as UserType[],
  })
 </script>
 
 <template>
+  <van-skeleton title avatar :row="3" :loading="props.loading" v-for="user in props.userList">
   <van-card
-      v-for="user in props.userList"
       :desc="user.profile"
       :title="`${user.username} (#${user.planetCode})`"
       :thumb="user.avatarUrl"
@@ -27,6 +29,7 @@ interface UserCardListProps{
       <van-button size="mini">联系我</van-button>
     </template>
   </van-card>
+  </van-skeleton>
 </template>
 
 <style scoped>
